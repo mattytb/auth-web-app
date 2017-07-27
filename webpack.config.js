c4e8d075d64b13e6f334b8b17a429fc4d5200b87
Matthew Barnden <matt@wambiz.com>
2017-07-27 13:01:02 -0400

    prep for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,5 @@
-module.exports = {
-  entry: [
-    './src/index.jsx'
-  ],
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    },
-    { 
-    	test: /\.css$/, 
-    	loader: "style-loader!css-loader" 
-    },
-    {
-      test: /\.(png|jpg|gif)$/,
-      loader: 'url-loader',
-      options: { limit: 8192}
-    }]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    
-    historyApiFallback: {
-      index: 'index.html'
-    },
-    contentBase: './dist'
-  }
+
+module.exports = function(env) {
+    return require(`./webpack.${env}.js`);
 };
+
