66712d7f912b6dfd669665e0f2e1984dd1fa3e7e
Matthew Barnden <matt@aheadmedia.co.uk>
2017-09-28 23:56:07 +0100

    server changes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var path = require('path');
 app = express();
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.resolve(__dirname, 'dist')));
+app.get('/showUsers', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+port);
