f1d134e8d66b4d528f24cf883140f6f488823f6a
Matthew Barnden <matt@aheadmedia.co.uk>
2017-09-28 21:32:47 +0100

    edits

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var path = require('path');
 app = express();
-app.use(express.static(path.resolve(__dirname, 'dist')));
-app.get('/showUsers', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, '/dist')));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+port);
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,34 +1,11 @@
-const webpack = require("webpack");
-let ExtractTextPlugin = require("extract-text-webpack-plugin");
-
 module.exports = {
-  entry: './src/index.jsx',
   output: {
     path: __dirname + '/dist',
+    publicPath: '/',
     filename: 'bundle.js'
   },
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    },
-    { 
-      test: /\.css$/, 
-      loader: "style-loader!css-loader" 
-    },
-    {
-      test: /\.(png|jpg|gif)$/,
-      loader: 'url-loader',
-      options: { limit: 8192}
-    }]
-  },
+
   plugins: [
-    new webpack.DefinePlugin({
-       'process.env': {
-           'NODE_ENV': JSON.stringify('production')
-       }
-    }),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
       debug: false
@@ -43,10 +20,6 @@ module.exports = {
         screw_ie8: true
       },
       comments: false
-    }),
-    new ExtractTextPlugin("styles.css")
-  ],
-  resolve: {
-      extensions: ['.js', '.jsx', '.css']
-  }
+    })
+  ]
 }
